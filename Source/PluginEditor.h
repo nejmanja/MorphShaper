ebc871829d67b3102c28a5e09458e10dcc72e955
Nemanja Milanović <neca1mesto@gmail.com>
2024-03-23 19:54:00 +0100

    Initial commit, .gitignore

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class MorphShaperAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    MorphShaperAudioProcessorEditor (MorphShaperAudioProcessor&);
+    ~MorphShaperAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    MorphShaperAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MorphShaperAudioProcessorEditor)
+};
